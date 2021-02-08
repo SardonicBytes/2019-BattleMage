26757493ead9ec3d5761fdb6fbe3154e6f766f27
SardonicBytes <Sardonicbytes@gmail.com>
2021-02-07 16:27:33 -0800

    Added Chess Graphics
    Added new sprites for chess.
    Modified existing environment and character sprites
    Created Chess Scene

---
+++ Assets/randomAnimationOffset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// For the love of all that is spaghetti Don't ever allow this quick hack into a final version
+public class randomAnimationOffset : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(DelayedStart());
+    }
+
+    IEnumerator DelayedStart()
+    {
+        yield return new WaitForSeconds(Random.Range(0f,2f));
+        anim.SetTrigger("Start");
+    }
+
+}
